0d711cc9318ba742c77e4cc51fded8446bb2c7a5
Alonso Vazquez <haloanderson45@gmail.com>
2021-01-04 15:29:07 -0600

    Adding CORS headers to allow "localhost".

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const colors = require("colors");
+const morgan = require("morgan");
+const path = require("path");
 
 const connectDB = require("./config/db");
 
@@ -12,11 +14,38 @@ const transactions = require("./routes/transactions");
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader(
+    "Access-Contro-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  next();
+});
+
 app.use(express.json());
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 const PORT = process.env.PORT || 5000;
 
 app.use("/api/v1/transactions", transactions);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 app.listen(
   PORT,
